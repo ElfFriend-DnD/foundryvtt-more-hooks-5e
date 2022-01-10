cfddfe8e38bdaedc487175bed13a44bc0b466b09
akrigline <a.krigline@gmail.com>
2022-01-09 21:25:14 -0500

    add applyDamage hook

--- setup.js
+++ setup.js
@@ -1,19 +1,21 @@
-import { MODULE_TITLE, MODULE_NAME } from "./scripts/const.js";
-import { patchRollAbilitySave } from './scripts/actor/rollAbilitySave.js'
-import { patchRollAbilityTest } from './scripts/actor/rollAbilityTest.js'
-import { patchRollDeathSave } from './scripts/actor/rollDeathSave.js'
-import { patchRollSkill } from './scripts/actor/rollSkill.js'
-import { patchRollDamage } from './scripts/item/damageRoll.js'
-import { patchRoll } from './scripts/item/roll.js'
-import { patchDisplayCard } from './scripts/item/displayCard.js'
-import { patchRollAttack } from './scripts/item/rollAttack.js'
-import { patchRollFormula } from './scripts/item/rollFormula.js'
-import { patchRollRecharge } from './scripts/item/rollRecharge.js'
-import { patchRollToolCheck } from './scripts/item/rollToolCheck.js'
+import { patchApplyDamage } from "./scripts/actor/damageTaken.js";
+import { patchRollAbilitySave } from './scripts/actor/rollAbilitySave.js';
+import { patchRollAbilityTest } from './scripts/actor/rollAbilityTest.js';
+import { patchRollDeathSave } from './scripts/actor/rollDeathSave.js';
+import { patchRollSkill } from './scripts/actor/rollSkill.js';
+import { MODULE_NAME, MODULE_TITLE } from "./scripts/const.js";
+import { patchRollDamage } from './scripts/item/damageRoll.js';
+import { patchDisplayCard } from './scripts/item/displayCard.js';
+import { patchRoll } from './scripts/item/roll.js';
+import { patchRollAttack } from './scripts/item/rollAttack.js';
+import { patchRollFormula } from './scripts/item/rollFormula.js';
+import { patchRollRecharge } from './scripts/item/rollRecharge.js';
+import { patchRollToolCheck } from './scripts/item/rollToolCheck.js';
 
 Hooks.on("setup", () => {
   console.log(`${MODULE_NAME} | Initializing ${MODULE_TITLE}`);
   // actor hooks
+  patchApplyDamage();
   patchRollAbilitySave();
   patchRollAbilityTest();
   patchRollDeathSave();
